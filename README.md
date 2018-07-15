12ce9835232f7d09da196dae4b6f0fd73e24a1ed
David Antliff <david@pitchblende.co.nz>
2018-07-15 12:12:38 +1200

    WIP.

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # esp32-freqcount-example
+
+## Introduction
+
+This is an example application for the [esp32-freqcount] component.
+
+It is written and tested for the [ESP-IDF](https://github.com/espressif/esp-idf) environment, using the xtensa-esp32-elf toolchain (gcc version 5.2.0).
+
+Ensure that submodules are cloned:
+
+    $ git clone --recursive https://github.com/DavidAntliff/esp32-freqcount-example.git
+
+Build the application with:
+
+    $ cd esp32-freqcount-example.git
+    $ make menuconfig    # set your serial configuration and IO configuration
+    $ make flash monitor
+
+The program should monitor pulses on the configured input and display the measured frequency in the console.
+
+## Dependencies
+
+This application makes use of the following components (included as submodules):
+
+ * components/[esp32-freqcount](https://github.com/DavidAntliff/esp32-freqcount)
+
+## Hardware
+
+TODO
+
+## Features
+
+This example provides:
+
+ * ...
+
+## Source Code
+
+The source is available from [GitHub](https://www.github.com/DavidAntliff/esp32-freqcount-example).
+
+## License
+
+The code in this project is licensed under the MIT license - see LICENSE for details.
+
+## Links
+
+ * [Espressif IoT Development Framework for ESP32](https://github.com/espressif/esp-idf)
+
+## Acknowledgements
+
+Thank you to [Chris Morgan](https://github.com/chmorgan) for converting the original demo application into a reusable IDF component, which is used by this example.
+
---
+++ main/app_main.c
@@ -0,0 +1,46 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 David Antliff
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ * @file app_main.c
+ * @brief Example application for the ESP32 Frequency Counter component..
+ */
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "sdkconfig.h"
+
+#include "frequency_count.h"
+
+#define TAG "app"
+
+void app_main()
+{
+    xTaskCreate(&frequency_count_task_function, "frequency_count_task", 2048, NULL, 5, NULL);
+}
+
